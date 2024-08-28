4c3ceb9c6a675d0744461df21d5b3b0921ca4d5c
robertmacyiii <robert.macyiii@gmail.com>
2024-08-27 22:57:51 -0700

    Add lualine plugin

--- init.lua
+++ init.lua
@@ -888,6 +888,7 @@ require('lazy').setup({
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.copilot',
   require 'kickstart.plugins.git-fugitive',
+  require 'kickstart.plugins.lualine',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
